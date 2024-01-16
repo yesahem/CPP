04756c9ff5ef4a6e1a46a0cac99715f0f03ed06c
iitanahem <ahemraj82@g>
2024-01-16 11:14:24 +0530

    Taking two input and performing additon

---
+++ second.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+  int num1, num2;
+  cout << "Enter the First Number: ";
+  cin >> num1;
+  cout << "Enter the second Number: ";
+  cin >> num2; // 'cin' function is already defined in iostrem header file used
+               // to take input and store it in the assigned variable
+  cout << "The sun of first and second number is: " << num1 + num2;
+  return 0;
+}
