54a740ee3c4b89d2b326b6cae57ac2814ea3deaa
iitanahem <ahemraj82@g>
2024-02-29 17:53:40 +0530

    Concept of pointer to object (#15)

---
+++ pointer_to_obj.cpp
@@ -0,0 +1,32 @@
+// in this we will take an example of pointer to fucntion
+
+#include <iostream>
+using namespace std;
+class student {
+public:
+  int test_var;
+
+  void set_test_var() {
+    int a;
+    cout << "Enter the number " << endl;
+    cin >> a;
+    test_var = a;
+  }
+  void get_test_var() {
+    cout << "The value of test var variable is: " << test_var << endl;
+  }
+};
+
+class man : public student {
+public:
+  int p;
+  void pata_nhi_bc() { cout << "Man class is called" << endl; }
+};
+int main(int argc, char *argv[]) {
+  student s1;
+  student *s;
+  s = &s1;
+  s->set_test_var();
+  s->get_test_var();
+  return 0;
+}
